7314276706a72914cc6122fd15b76cabe9ae27f4
超神的冰凉 <2801621420@qq.com>
2022-08-05 22:58:58 +0800

    初始化

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-/*
- * 整个JS代码将在服务器刚启动时加载，请务必牢记：大部分与游戏相关的内容，
- * 如监听事件，生成物品，操作地图，控制生物等等都不能在刚开始的时候进行！
- * 
- * The entire JS code will be loaded when the server is just started. 
- * Please keep in mind that most game related content, such as monitoring events, 
- * generating items, operating worlds, controlling entities, etc., cannot be carried out at the beginning!
- */
-console.log("Wow world")
+import { PowerNukkitX, EventPriority } from ":powernukkitx";
+import { BlockItemUtil, CreativeInventoryType, ToolType, ToolTier } from ":util";
 
-/*
- * main函数将会在PNX服务器初始化完成后被调用，此函数被调用时及以后您可以进行与游戏相关的操作。
-
- * The main function will be called after the PNX server is initialized. 
- * When this function is called and later, you can perform game related operations.
- */
 export function main() {
-    console.warn("Hello world")
+    console.warn("Hello world");
+    BlockItemUtil.registerSimpleItem("emergency_food:primogem_juice", "Primogem Juice",
+        CreativeInventoryType.ITEM, "juice", 16, 32, true);
+    BlockItemUtil.registerFoodItem("emergency_food:paimeng", "PaiMeng",
+        CreativeInventoryType.ITEM, "paimeng", 16, 1, true,
+        200, false, true, 6, 8);
+    BlockItemUtil.registerToolItem("emergency_food:freedom_sworn", "Freedom Sworn",
+        CreativeInventoryType.ITEM, "freedom_sworn", 128, 1, true,
+        ToolType.SWORD, ToolTier.DIAMOND, 6, true, true);
 }
 
-/*
- * close函数将会在在PNX服务器关闭时或您的插件被卸载时调用，此时您应当进行一些清理收尾工作。
- * The close function will be called when the PNX server is shutdown or your plugin is uninstalled. 
- * At this time, you should do some cleaning and finishing work.
- */
 export function close() {
     console.error("Goodbye world")
 }
